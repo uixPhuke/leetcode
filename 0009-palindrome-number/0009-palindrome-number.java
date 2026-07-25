78c1bdad9cc71edcb58ebc49cd481e4687673856
Ruhon Borah <uixphuke@gmail.com>
2026-07-25 10:46:47 +0530

    Time: 5 ms (80.95%), Space: 45.9 MB (72.55%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rem,rev=0;
+        int temp=x;
+        while(x>0){
+            rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+
+        }
+        if(temp==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
