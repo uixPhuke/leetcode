921bff0df09f0ccd7f0f591597f261943fd757c8
Ruhon Borah <uixphuke@gmail.com>
2026-09-12 23:28:09 +0530

    Time: 2 ms (63.66%), Space: 43.7 MB (15.56%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -3,12 +3,6 @@ class Solution {
         if(s.length()!=goal.length()){
             return false;
         }
-        for(int i=0;i<s.length();i++){
-            if(s.equals(goal)){
-                return true;
-            }
-            s=s.substring(1)+s.charAt(0);
-        }
-        return false;
+       return (s+s).contains(goal);
     }
 }
