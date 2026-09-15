a0d58955223d4e154b8771d96df443b483fdc5ef
Ruhon Borah <uixphuke@gmail.com>
2026-09-15 19:13:44 +0530

    Time: 3 ms (88.83%), Space: 46.3 MB (16.51%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -1,18 +1,11 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
        int max=0;
-       for(String sentence:sentences){
-            int words=1;
-            for(int i=0;i<sentence.length();i++){
-                if(sentence.charAt(i)==' '){
-                    words++;
-                }
-                
-            }
-            max=Math.max(max,words);
-            
-       }
-       return max;
+      for(String sentence:sentences){
+        int words=sentence.split(" ").length;
+        max=Math.max(max,words);
+      }
+      return max;
 
     }
 }
