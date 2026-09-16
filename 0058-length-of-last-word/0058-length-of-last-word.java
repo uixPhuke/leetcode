79fe150621fcd06bb6e9720bf23fe1b1338e1676
Ruhon Borah <uixphuke@gmail.com>
2026-09-17 00:22:20 +0530

    Time: 1 ms (26.48%), Space: 43.2 MB (16.1%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,18 +1,8 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int i=s.length()-1;
-        int count=0;
-        while(i>0 && s.charAt(i)==' '){
-           
-                i--;   
-        }
-        while(i>=0 && s.charAt(i)!=' '){
-           
-               i--;
-               count++;
-               
-        }
-        return count;
 
+        String[] words = s.trim().split(" ");
+
+        return words[words.length - 1].length();
     }
 }
