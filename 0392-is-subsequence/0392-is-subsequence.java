ca31abcd0727f191fb2030b57720a5a9e1177a37
Ruhon Borah <uixphuke@gmail.com>
2026-09-17 10:41:29 +0530

    Time: 2 ms (72.99%), Space: 42.7 MB (80.18%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+                int left=0;
+                int right=0;
+                while(left<s.length() && right<t.length()){
+                    if(s.charAt(left)==t.charAt(right)){
+                        left++;
+                    }
+                    right++;
+                }
+                if(left==s.length()){
+                    return true;
+                }
+                return false;
+
+    }
+
+}
