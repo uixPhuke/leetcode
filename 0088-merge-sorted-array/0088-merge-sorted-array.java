e8f3952fc88645c8c1d67d748b30b2505a8d6518
Ruhon Borah <uixphuke@gmail.com>
2026-09-17 22:40:02 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.37%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,31 +1,41 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] result=new int[m+n];
-        int i=m-1,j=n-1,k=m+n-1;
 
-        while(i>=0 && j>=0){
-                if(nums1[i]>nums2[j]){
-                   nums1[k]=nums1[i];
-                    i--;
+        int[] left = new int[m];
+        int[] right = new int[n];
 
-                }
-                 else{
-                     nums1[k]=nums2[j];
-                    j--;
-
-                }
-                k--;
-            }
-            while(j>=0 ){
-                
-                   nums1[k]=nums2[j];
-                    k--;
-                    j--;
-
-                }
-               
-               
-
+      
+        for (int i = 0; i < m; i++) {
+            left[i] = nums1[i];
         }
 
+      
+        for (int j = 0; j < n; j++) {
+            right[j] = nums2[j];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+      
+        while (i < m && j < n) {
+
+            if (left[i] <= right[j]) {
+                nums1[k++] = left[i++];
+            } 
+            else {
+                nums1[k++] = right[j++];
+            }
+        }
+
+        
+        while (i < m) {
+            nums1[k++] = left[i++];
+        }
+
+        while (j < n) {
+            nums1[k++] = right[j++];
+        }
     }
+}
